8c7588196eca6fdbfb13583921dd0be342211745
Marviel <lukebechtel4@gmail.com>
2023-09-28 14:17:45 -0400

    lint fixes

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -4,6 +4,22 @@ module.exports = {
     "mui-path-imports"
   ],
   "rules": {
-      "mui-path-imports/mui-path-imports": "error"
+      "mui-path-imports/mui-path-imports": "error",
+      "unicorn/filename-case": 'off',
+      "@typescript-eslint/no-unnecessary-condition": "warn",
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unnecessary-condition": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-empty-function": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "no-console": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/no-misused-promises": "warn",
+      "@typescript-eslint/no-useless-constructor": "warn",
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "@typescript-eslint/ban-types": "warn",
   }
 };
